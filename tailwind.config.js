f3183646f136282567aeb0040b321e77cba39bf9
unknown <Anirat04>
2024-07-31 17:01:21 +0600

    Tailwind config file customised with required colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        textPrimary: "#696B76",
+        textSecondHeading: "#87888D",
+        testimonialUserText: "#CACACA",
+        menuTextCol: "#a0a1a5",
+        pricePlanPrimaryText: "#606266",
+        pricePlanSecondaryText: "#8C8F93"
+      },
+      backgroundColor:{
+        bgPrimary: "#1E2029",
+        bodyBg: "#12141D",
+        bgPrimaryOrange: "#FF664F"
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
 }
 
